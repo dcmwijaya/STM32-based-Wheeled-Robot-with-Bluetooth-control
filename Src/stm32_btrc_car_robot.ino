0010d3ec1e6c7d91b4d1b2676198dc145431d197
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-01-22 07:01:08 +0700

    Update stm32_btrc_car_robot.ino

--- Src/stm32_btrc_car_robot.ino
+++ Src/stm32_btrc_car_robot.ino
@@ -1,4 +1,3 @@
-#include <SoftwareSerial.h> // This library is used for serial communication
 const int enable_motorA = PA1; // PA1 pin to enable motor A
 const int motorA1 = PA2; // PA2 pin to rotate the right motor direction forward
 const int motorA2 = PA3; // PA3 pin to rotate the right motor direction backward
@@ -7,12 +6,10 @@ const int motorB2 = PB7; // PB7 pin to rotate the left motor direction forward
 const int enable_motorB = PB8; // PB8 pin to enable motor B
 
 String cmd; // Initialize a cmd variable with string data type
-SoftwareSerial SPPC(PA10, PA9); // STM32 pin initialization => RX: PA10, TX: PA9
 
 // Methods setup
 void setup(){
   Serial.begin(9600); // Initiate serial communication at baudrate 9600
-  SPPC.begin(9600); // Initiate bluetooth sensor
   pinMode(enable_motorA, OUTPUT); // PA1 pin is used as an output
   pinMode(motorA1, OUTPUT); // PA2 pin is used as an output
   pinMode(motorA2, OUTPUT); // PA3 pin is used as an output
@@ -24,7 +21,7 @@ void setup(){
 // Methods loop
 void loop(){  
   analogWrite(enable_motorA, 200); analogWrite(enable_motorB, 200); // Controlling speed (0 = off and 255 = max speed)
-  while(SPPC.available()>0){ cmd = SPPC.read(); } // Bluetooth sensor readings
+  if(Serial.available()>0){ cmd = Serial.read(); } // Bluetooth sensor readings
   if(cmd == "F"){ forward(); } // If the button whose value is "F" is pressed, the robot car will move forward
   if(cmd == "B"){ backward(); } // If the button whose value is "B" is pressed, the robot car will move backward
   if(cmd == "L"){ left(); } // If the button whose value is "L" is pressed, the robot car will turn left
